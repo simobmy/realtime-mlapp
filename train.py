7719318698b63c47afbfdbce1f66f8f65e3fc992
shdowhacker <s.boumedyane@gmail.com>
2021-02-28 19:46:01 +0100

    Add Train.py

---
+++ train.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import plot_confusion_matrix
+import warnings
+import pickle
+warnings.filterwarnings("ignore")
+
+seed = 42
+
+df = pd.read_csv("features_data.csv")
+data = np.array(df)
+
+df_features = df[['birth_year', 'country', 'city', 'plan', 'num_contacts',
+       'User_gain_usd', 'Transaction_per_user',
+       'notification_received', 'target']]
+
+y = df_features.target
+X = df_features.drop('target', axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3, random_state=seed)
+# Create the Decision Tree classifier with the optimal hyperparameters as found by GridSearchCV
+tree_clf = Pipeline([
+    ('select_features', SelectKBest(k=7)),
+    ('classify', DecisionTreeClassifier(criterion='entropy', max_depth=5, max_features=None, min_samples_split=20))
+])
+
+tree_clf.fit(X_train, y_train)
+y_pred = tree_clf.predict(X_test)
+score = tree_clf.score(X_test, y_test)
+print (score)
+
+with open("metrics.txt", 'w') as outfile:
+        outfile.write("Accuracy: " + str(score) + "\n")
+
+
+# Plot it
+disp = plot_confusion_matrix(tree_clf, X_test, y_test, normalize='true',cmap=plt.cm.Blues)
+plt.savefig('confusion_matrix.png')
+
+pickle.dump(tree_clf,open('my_model.pkl','wb'))

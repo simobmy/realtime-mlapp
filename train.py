a7a45d389ff599b1b744381b1f8c1ee27cc85211
shdowhacker <s.boumedyane@gmail.com>
2021-02-28 20:17:04 +0100

    Update

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest
